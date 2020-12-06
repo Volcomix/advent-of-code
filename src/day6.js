e3fb1241a2e6f90c91ed0bbd47bb84bea982d34c
Volcomix <Volcomix@users.noreply.github.com>
2020-12-06 11:06:26 +0100

    Day 6 part 2

--- src/day6.js
+++ src/day6.js
@@ -5,7 +5,12 @@ async function readAnswers() {
   return input
     .toString()
     .split('\n\n')
-    .map((group) => group.split('\n').map((answer) => answer.split('')))
+    .map((group) =>
+      group
+        .split('\n')
+        .filter((line) => line)
+        .map((answer) => answer.split('')),
+    )
 }
 
 async function part1() {
@@ -18,4 +23,23 @@ async function part1() {
   )
 }
 
+async function part2() {
+  const answers = await readAnswers()
+  console.log(
+    'Part 2:',
+    answers
+      .map((group) => {
+        const groupSize = group.length
+        const answerCount = new Map()
+        for (const answer of group.flat()) {
+          answerCount.set(answer, (answerCount.get(answer) || 0) + 1)
+        }
+        return [...answerCount.values()].filter((count) => count === groupSize)
+          .length
+      })
+      .reduce((sum, count) => sum + count, 0),
+  )
+}
+
 part1()
+part2()
