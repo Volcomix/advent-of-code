b957ff96e6851dbfa64cb404a99b999fa5d73886
Volcomix <Volcomix@users.noreply.github.com>
2020-12-09 10:14:05 +0100

    Day 9 part 2

--- src/day9.js
+++ src/day9.js
@@ -5,8 +5,10 @@ async function readXmas() {
   return xmas.split('\n').map(Number)
 }
 
-async function part1() {
-  const xmas = await readXmas()
+/**
+ * @param {number[]} xmas
+ */
+function findInvalidNumberIndex(xmas) {
   const preambleSize = 25
   for (let i = preambleSize; i < xmas.length; i++) {
     let isFound = false
@@ -18,10 +20,48 @@ async function part1() {
       }
     }
     if (!isFound) {
-      console.log(xmas[i])
+      return i
+    }
+  }
+}
+
+async function part1() {
+  const xmas = await readXmas()
+  console.log('Part 1:', xmas[findInvalidNumberIndex(xmas)])
+}
+
+async function part2() {
+  const xmas = await readXmas()
+  const invalidNumberIndex = findInvalidNumberIndex(xmas)
+  const invalidNumber = xmas[invalidNumberIndex]
+  for (let i = 0; i < xmas.length; i++) {
+    if (i === invalidNumberIndex) {
+      continue
+    }
+    let sum = 0
+    let min = Infinity
+    let max = -Infinity
+    let j = i
+    for (
+      ;
+      j < xmas.length && j !== invalidNumberIndex && sum < invalidNumber;
+      j++
+    ) {
+      const value = xmas[j]
+      sum += value
+      if (value < min) {
+        min = value
+      }
+      if (value > max) {
+        max = value
+      }
+    }
+    if (sum === invalidNumber) {
+      console.log('Part 2:', min + max)
       break
     }
   }
 }
 
 part1()
+part2()
