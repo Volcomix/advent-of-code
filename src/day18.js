b2cc59dc27ddcd62db7d1d1ced8efe6164a7a3d9
Volcomix <Volcomix@users.noreply.github.com>
2020-12-18 09:33:35 +0100

    Day 18 part 2

--- src/day18.js
+++ src/day18.js
@@ -10,17 +10,38 @@ async function readExpressions() {
 /**
  * @param {string} expression
  */
-function resolve(expression) {
+function resolve1(expression) {
   if (/\(\d+\)/.test(expression)) {
-    return resolve(expression.replace(/\((\d+)\)/, '$1'))
+    return resolve1(expression.replace(/\((\d+)\)/, '$1'))
   }
   if (/[\+\*]/.test(expression)) {
-    return resolve(
+    return resolve1(
       expression.replace(
         /\((\d+[\+\*]\d+)([\d\+\*]*)\)/,
         (_, p1, p2) => `(${eval(p1)}${p2})`,
       ),
     )
+  }
+  return expression
+}
+
+/**
+ * @param {string} expression
+ */
+function resolve2(expression) {
+  if (/\(\d+\)/.test(expression)) {
+    return resolve2(expression.replace(/\((\d+)\)/, '$1'))
+  }
+  if (/\([\d\+\*]+\)/.test(expression)) {
+    return resolve2(
+      expression.replace(/\(([\d\+\*]+)\)/, (_, p1) => resolve2(p1)),
+    )
+  }
+  if (/\d+\+\d+/.test(expression)) {
+    return resolve2(expression.replace(/\d+\+\d+/, eval))
+  }
+  if (/\d+\*\d+/.test(expression)) {
+    return resolve2(expression.replace(/\d+\*\d+/, eval))
   }
   return expression
 }
@@ -31,10 +52,22 @@ async function part1() {
   console.log(
     'Part 1:',
     expressions.reduce(
-      (sum, expression) => sum + Number(resolve(expression)),
+      (sum, expression) => sum + Number(resolve1(expression)),
+      0,
+    ),
+  )
+}
+
+async function part2() {
+  const expressions = await readExpressions()
+  console.log(
+    'Part 2:',
+    expressions.reduce(
+      (sum, expression) => sum + Number(resolve2(expression)),
       0,
     ),
   )
 }
 
 part1()
+part2()
