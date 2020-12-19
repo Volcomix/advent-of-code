655f6eac11589d38dd43630cb9bc450e26ea6279
Volcomix <Volcomix@users.noreply.github.com>
2020-12-19 11:05:08 +0100

    Day 19 part 2

--- src/day19.js
+++ src/day19.js
@@ -26,49 +26,77 @@ async function readInput() {
  * @param {number} index
  * @param {number} ruleId
  * @param {Rules} rules
- * @returns {number}
+ * @returns {number[]}
  */
 function matches(message, index, ruleId, rules) {
-  for (const subRuleOr of rules[ruleId]) {
-    let matchingLength = 0
-    for (const subRuleAnd of subRuleOr) {
-      if (
-        typeof subRuleAnd === 'string' &&
-        message[index + matchingLength] === subRuleAnd
-      ) {
-        matchingLength++
-      } else if (typeof subRuleAnd === 'number') {
-        const subMatchingLength = matches(
-          message,
-          index + matchingLength,
-          subRuleAnd,
-          rules,
-        )
-        if (subMatchingLength === 0) {
-          matchingLength = 0
+  return rules[ruleId]
+    .flatMap((subRuleOr) => {
+      let matchingLengths = [0]
+      for (const subRuleAnd of subRuleOr) {
+        matchingLengths = matchingLengths.flatMap((matchingLength) => {
+          if (
+            typeof subRuleAnd === 'string' &&
+            message[index + matchingLength] === subRuleAnd
+          ) {
+            return [matchingLength + 1]
+          } else if (typeof subRuleAnd === 'number') {
+            const subMatchingLengths = matches(
+              message,
+              index + matchingLength,
+              subRuleAnd,
+              rules,
+            )
+            if (subMatchingLengths.length === 0) {
+              return []
+            }
+            return subMatchingLengths.map(
+              (subMatchingLength) => matchingLength + subMatchingLength,
+            )
+          } else {
+            return []
+          }
+        })
+        if (matchingLengths.length === 0) {
           break
         }
-        matchingLength += subMatchingLength
-      } else {
-        matchingLength = 0
-        break
       }
-    }
-    if (matchingLength > 0) {
-      return matchingLength
-    }
-  }
-  return 0
+      return matchingLengths
+    })
+    .filter((matchingLength) => matchingLength)
 }
 
 async function part1() {
   const { rules, messages } = await readInput()
   const matchCount = messages.reduce(
     (count, message) =>
-      matches(message, 0, 0, rules) === message.length ? count + 1 : count,
+      matches(message, 0, 0, rules).find(
+        (matchingLength) => matchingLength === message.length,
+      )
+        ? count + 1
+        : count,
     0,
   )
   console.log('Part 1:', matchCount)
 }
 
+async function part2() {
+  const { rules, messages } = await readInput()
+  rules[8] = [[42], [42, 8]]
+  rules[11] = [
+    [42, 31],
+    [42, 11, 31],
+  ]
+  const matchCount = messages.reduce(
+    (count, message) =>
+      matches(message, 0, 0, rules).find(
+        (matchingLength) => matchingLength === message.length,
+      )
+        ? count + 1
+        : count,
+    0,
+  )
+  console.log('Part 2:', matchCount)
+}
+
 part1()
+part2()
