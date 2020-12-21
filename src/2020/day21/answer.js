b68509000447563d403da055b8c895d0f529a2b7
Volcomix <Volcomix@users.noreply.github.com>
2020-12-21 11:07:49 +0100

    Day 21 part 2

--- src/2020/day21/answer.js
+++ src/2020/day21/answer.js
@@ -1,6 +1,6 @@
 import { readFile } from '../../file-helper.js'
 
-async function part1() {
+async function main() {
   const input = await readFile()
   const foods = input
     .split('\n')
@@ -10,31 +10,58 @@ async function part1() {
       allergens: allergens.split(', '),
     }))
 
-  /** @type {Map<string, string[]>} */
+  /** @type {Map<string, Set<string>>} */
   const allergensMap = new Map()
 
   for (const food of foods) {
     for (const allergen of food.allergens) {
+      const newAllergenIngredients = new Set(food.ingredients)
       if (allergensMap.has(allergen)) {
-        const allergenIngredients = new Set(food.ingredients)
         allergensMap.set(
           allergen,
-          allergensMap
-            .get(allergen)
-            .filter((ingredient) => allergenIngredients.has(ingredient)),
+          new Set(
+            [...allergensMap.get(allergen)].filter((ingredient) =>
+              newAllergenIngredients.has(ingredient),
+            ),
+          ),
         )
       } else {
-        allergensMap.set(allergen, food.ingredients)
+        allergensMap.set(allergen, newAllergenIngredients)
       }
     }
   }
 
-  const ingredientsWithAllergen = new Set([...allergensMap.values()].flat())
+  const ingredientsWithAllergen = new Set(
+    [...allergensMap.values()].flatMap((ingredients) => [...ingredients]),
+  )
   const ingredientsWithoutAllergen = foods
     .flatMap((food) => food.ingredients)
     .filter((ingredient) => !ingredientsWithAllergen.has(ingredient))
 
   console.log('Part 1:', ingredientsWithoutAllergen.length)
+
+  /** @type {Map<string, string>} */
+  const ingredientsMap = new Map()
+
+  while (allergensMap.size) {
+    const [allergen, ingredients] = [...allergensMap].find(
+      ([_, ingredients]) => ingredients.size === 1,
+    )
+    const ingredient = ingredients.values().next().value
+    ingredientsMap.set(ingredient, allergen)
+    allergensMap.delete(allergen)
+    for (const [_, ingredients] of allergensMap) {
+      ingredients.delete(ingredient)
+    }
+  }
+
+  const ingredients = [...ingredientsMap]
+    .sort(([_ingredient1, allergen1], [_ingredient2, allergen2]) =>
+      allergen1 > allergen2 ? 1 : allergen1 < allergen2 ? -1 : 0,
+    )
+    .map(([ingredient]) => ingredient)
+
+  console.log('Part 2:', ingredients.join(','))
 }
 
-part1()
+main()
