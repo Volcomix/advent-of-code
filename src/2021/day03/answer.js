001d7d30277f64b2f8f893312efb115636b8ce8b
Volcomix <Volcomix@users.noreply.github.com>
2021-12-04 09:27:42 +0100

    2021 day 3 part 2

--- src/2021/day03/answer.js
+++ src/2021/day03/answer.js
@@ -1,5 +1,35 @@
 import { readFile } from '../../file-helper.js'
 
+/**
+ * @param {string[]} lines
+ * @param {number} bit
+ */
+function bitValueCounts(lines, bit) {
+  const counts = [0, 0]
+  for (const line of lines) {
+    counts[Number(line[bit])]++
+  }
+  return counts
+}
+
+/**
+ * @param {string[]} lines
+ * @param {number} bit
+ */
+function mostCommonBitValue(lines, bit) {
+  const counts = bitValueCounts(lines, bit)
+  return counts[0] > counts[1] ? 0 : 1
+}
+
+/**
+ * @param {string[]} lines
+ * @param {number} bit
+ */
+function leastCommonBitValue(lines, bit) {
+  const counts = bitValueCounts(lines, bit)
+  return counts[0] <= counts[1] ? 0 : 1
+}
+
 async function part1() {
   const input = await readFile()
   const lines = input.split('\n')
@@ -8,11 +38,7 @@ async function part1() {
 
   let gammaRateBin = ''
   for (let bit = 0; bit < length; bit++) {
-    let counts = [0, 0]
-    for (const line of lines) {
-      counts[Number(line[bit])]++
-    }
-    gammaRateBin += counts[0] > counts[1] ? '0' : '1'
+    gammaRateBin += mostCommonBitValue(lines, bit)
   }
   const gammaRate = parseInt(gammaRateBin, 2)
 
@@ -25,4 +51,28 @@ async function part1() {
   console.log('Part 1:', gammaRate * epsilonRate)
 }
 
+async function part2() {
+  const input = await readFile()
+  const lines = input.split('\n')
+
+  const length = lines[0].length
+
+  let remainingLines = [...lines]
+  for (let bit = 0; bit < length && remainingLines.length > 1; bit++) {
+    const bitCriteria = mostCommonBitValue(remainingLines, bit).toString()
+    remainingLines = remainingLines.filter((line) => line[bit] === bitCriteria)
+  }
+  const oxygenGeneratorRating = parseInt(remainingLines[0], 2)
+
+  remainingLines = [...lines]
+  for (let bit = 0; bit < length && remainingLines.length > 1; bit++) {
+    const bitCriteria = leastCommonBitValue(remainingLines, bit).toString()
+    remainingLines = remainingLines.filter((line) => line[bit] === bitCriteria)
+  }
+  const CO2ScrubberRating = parseInt(remainingLines[0], 2)
+
+  console.log('Part 2:', oxygenGeneratorRating * CO2ScrubberRating)
+}
+
 part1()
+part2()
