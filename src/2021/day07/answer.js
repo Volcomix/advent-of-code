63c1c729dba0fc2180adfe17f0f7c2b080fed62c
Volcomix <Volcomix@users.noreply.github.com>
2021-12-07 09:15:40 +0100

    2021 day 7 part 2

--- src/2021/day07/answer.js
+++ src/2021/day07/answer.js
@@ -16,7 +16,22 @@ async function part1() {
   console.log('Part 1:', lowestDifferenceSum)
 }
 
-async function part2() {}
+async function part2() {
+  const positions = await readInput()
+  const maxPosition = Math.max(...positions)
+  const costs = [0]
+  for (let i = 1; i <= maxPosition; i++) {
+    costs[i] = costs[i - 1] + i
+  }
+  let lowestCostSum = Infinity
+  for (let i = 0; i <= maxPosition; i++) {
+    const costSum = sumCosts(positions, i, costs)
+    if (costSum < lowestCostSum) {
+      lowestCostSum = costSum
+    }
+  }
+  console.log('Part 2:', lowestCostSum)
+}
 
 async function readInput() {
   const input = await readFile()
@@ -29,4 +44,16 @@ async function readInput() {
  */
 function sumDifferences(positions, position) {
   return positions.reduce((acc, curr) => acc + Math.abs(curr - position), 0)
+}
+
+/**
+ * @param {number[]} positions
+ * @param {number} position
+ * @param {number[]} costs
+ */
+function sumCosts(positions, position, costs) {
+  return positions.reduce(
+    (acc, curr) => acc + costs[Math.abs(curr - position)],
+    0,
+  )
 }
