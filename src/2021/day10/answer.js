c0f97239e42e2f20da14398fc4f9d9b698a89d35
Volcomix <Volcomix@users.noreply.github.com>
2021-12-10 09:32:02 +0100

    2021 day 10 part 2

--- src/2021/day10/answer.js
+++ src/2021/day10/answer.js
@@ -1,24 +1,28 @@
 import { readFile } from '../../file-helper.js'
 
+const openingChars = new Map([
+  ['(', ')'],
+  ['[', ']'],
+  ['{', '}'],
+  ['<', '>'],
+])
+
+const closingChars = new Map(
+  [...openingChars.entries()].map(([opening, closing]) => [closing, opening]),
+)
+
 await part1()
 await part2()
 
 async function part1() {
   const lines = await readInput()
-  const openingChars = new Set(['(', '[', '{', '<'])
-  const openingCharsByClosingChar = new Map([
-    [')', '('],
-    [']', '['],
-    ['}', '{'],
-    ['>', '<'],
-  ])
   const illegalChars = []
   for (const line of lines) {
     const opened = []
     for (const char of line) {
       if (openingChars.has(char)) {
         opened.push(char)
-      } else if (opened.pop() !== openingCharsByClosingChar.get(char)) {
+      } else if (opened.pop() !== closingChars.get(char)) {
         illegalChars.push(char)
         break
       }
@@ -37,7 +41,44 @@ async function part1() {
   console.log('Part 1:', score)
 }
 
-async function part2() {}
+async function part2() {
+  const lines = await readInput()
+  const completions = []
+  for (const line of lines) {
+    const opened = []
+    let corrupted = false
+    for (const char of line) {
+      if (openingChars.has(char)) {
+        opened.push(char)
+      } else if (opened.pop() !== closingChars.get(char)) {
+        corrupted = true
+        break
+      }
+    }
+    if (corrupted) {
+      continue
+    }
+    if (opened.length > 0) {
+      completions.push(opened.reverse().map((char) => openingChars.get(char)))
+    }
+  }
+  const scoresByChar = new Map([
+    [')', 1],
+    [']', 2],
+    ['}', 3],
+    ['>', 4],
+  ])
+  const scores = []
+  for (const completion of completions) {
+    let score = 0
+    for (const char of completion) {
+      score = score * 5 + scoresByChar.get(char)
+    }
+    scores.push(score)
+  }
+  scores.sort((a, b) => a - b)
+  console.log('Part 2:', scores[Math.floor(scores.length / 2)])
+}
 
 async function readInput() {
   const input = await readFile()
