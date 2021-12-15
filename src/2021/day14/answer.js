40d67cc252b8dde7fd66c40a24ea15b28e1e302c
Volcomix <Volcomix@users.noreply.github.com>
2021-12-15 13:23:52 +0100

    2021 day 14 part 2

--- src/2021/day14/answer.js
+++ src/2021/day14/answer.js
@@ -25,21 +25,27 @@ async function part1() {
 
 async function part2() {
   const { template, pairs } = await readInput()
-  const pairMap = new Map()
-  for (const [pair, insertion] of pairs) {
-    const element = pairMap.get(pair[0]) ?? new Map()
-    element.set(pair[1], insertion)
-    pairMap.set(pair[0], element)
-  }
   const countsByElement = new Map()
-  for (let i = 0; i < template.length; i++) {
-    const element = template[i]
+  for (const element of template) {
     countsByElement.set(element, (countsByElement.get(element) ?? 0) + 1)
   }
+  let polymer = new Map([...pairs.keys()].map((pair) => [pair, 0]))
   for (let i = 1; i < template.length; i++) {
-    const [element1, element2] = template.substr(i - 1, 2)
-    traverse(element1, element2, pairMap, 40, countsByElement)
-    console.log('pair done', element1, element2, countsByElement)
+    const pair = template.substr(i - 1, 2)
+    polymer.set(pair, polymer.get(pair) + 1)
+  }
+  for (let i = 0; i < 40; i++) {
+    const nextPolymer = new Map(polymer)
+    for (const [pair, count] of polymer) {
+      const element = pairs.get(pair)
+      countsByElement.set(element, (countsByElement.get(element) ?? 0) + count)
+      const pair1 = `${pair[0]}${element}`
+      const pair2 = `${element}${pair[1]}`
+      nextPolymer.set(pair1, nextPolymer.get(pair1) + count)
+      nextPolymer.set(pair2, nextPolymer.get(pair2) + count)
+      nextPolymer.set(pair, nextPolymer.get(pair) - count)
+    }
+    polymer = nextPolymer
   }
   const counts = [...countsByElement.values()].sort((a, b) => a - b)
   console.log('Part 2:', counts[counts.length - 1] - counts[0])
