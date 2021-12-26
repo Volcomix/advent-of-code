78590711a31933e3c4040b8cc82c789557fa9bbc
Volcomix <Volcomix@users.noreply.github.com>
2021-12-26 11:49:03 +0100

    2021 day 21 part 2

--- src/2021/day21/answer.js
+++ src/2021/day21/answer.js
@@ -9,7 +9,7 @@ async function part1() {
   let rollCount = 0
   let die = 1
   while (true) {
-    for (let player = 0; player < positions.length; player++) {
+    for (let player = 0; player < 2; player++) {
       let rolls = 0
       for (let i = 0; i < 3; i++) {
         rolls += die++
@@ -28,7 +28,24 @@ async function part1() {
   }
 }
 
-async function part2() {}
+async function part2() {
+  const positions = await readInput()
+
+  const universeCounts = new Map(
+    Array.from({ length: 7 }, (_, i) => [i + 3, 0]),
+  )
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+      for (let k = 1; k <= 3; k++) {
+        const die = i + j + k
+        universeCounts.set(die, universeCounts.get(die) + 1)
+      }
+    }
+  }
+
+  const wins = dfs(0, 1, universeCounts, positions, [0, 0], [0, 0])
+  console.log('Part 2:', Math.max(...wins))
+}
 
 async function readInput() {
   const input = await readFile()
@@ -36,4 +53,34 @@ async function readInput() {
     .split('\n')
     .map((line) => line.split(': ').pop())
     .map(Number)
+}
+
+/**
+ * @param {number} player
+ * @param {number} universeCount
+ * @param {Map<number, number>} universeCounts
+ * @param {number[]} positions
+ * @param {number[]} scores
+ * @param {number[]} wins
+ */
+function dfs(player, universeCount, universeCounts, positions, scores, wins) {
+  if (scores[1 - player] >= 21) {
+    wins[1 - player] += universeCount
+    return wins
+  }
+  for (let moveCount = 3; moveCount <= 9; moveCount++) {
+    const nextPositions = [...positions]
+    const nextScores = [...scores]
+    nextPositions[player] = ((nextPositions[player] + moveCount - 1) % 10) + 1
+    nextScores[player] += nextPositions[player]
+    dfs(
+      1 - player,
+      universeCount * universeCounts.get(moveCount),
+      universeCounts,
+      nextPositions,
+      nextScores,
+      wins,
+    )
+  }
+  return wins
 }
